fe881a47d486c53e199d38927e2fe32831c7cdea
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-01 15:09:11 +0000

    PrimeTime part 2

--- src/AmarpalAmrith/JaveMiniProblems/PrimeTime.java
+++ src/AmarpalAmrith/JaveMiniProblems/PrimeTime.java
@@ -4,19 +4,20 @@ public class PrimeTime {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Prime Time!");
-        Utilities.msgPrompt("Please enter a number to check if it is prime");
+        Utilities.msgPrompt("Please enter a number to check if any number upto it is prime");
         int userInput = Utilities.condNo(1);
-        System.out.println("Checking " + userInput + " is prime...");
-
-            if (checkifPrime(userInput)) {
-                System.out.println(userInput + " is a prime number!");
-                //System.exit(-1);
-            } else {
-                System.out.println(userInput + " is not a prime number!");
+        System.out.println("Checking every number upto " + userInput);
+        for (int i = 1; i < userInput; i++) {
+            if (checkifPrime(i)) {
+                System.out.println(i + " is a prime number!");
             }
+            //else {
+                //System.out.println(userInput + " is not a prime number!");
+            //}
+        }
     }
 
-    public static boolean checkifPrime(int input) {
+    public static boolean checkifPrime (int input) {
         for (int i = 2; i < input; i++) {
             if (input % i == 0) {
                 return false;
