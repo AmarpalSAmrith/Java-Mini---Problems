4c408bfe3cf9e84f8d6271b5678ab379a4661605
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-01 10:35:57 +0000

    Summing up, Part 1

--- src/AmarpalAmrith/JaveMiniProblems/Main.java
+++ src/AmarpalAmrith/JaveMiniProblems/Main.java
@@ -1,20 +1,7 @@
 package AmarpalAmrith.JaveMiniProblems;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Greetings!");
-        System.out.println("==========");
-        System.out.println("Please input your name :)");
-        System.out.print(">");
-        String input = scanner.next();
-
-        if (input.equals("Alice") || input.equals("Bob")) {
-            System.out.print("Greetings " + input + ", How are you?");
-        } else
-            System.out.println("This Program does not know you.");
     }
 }
---
+++ src/AmarpalAmrith/JaveMiniProblems/SummingUp.java
@@ -0,0 +1,21 @@
+package AmarpalAmrith.JaveMiniProblems;
+
+import java.util.Scanner;
+
+public class SummingUp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yo! Wagwan dude!");
+        System.out.println("================");
+        System.out.println("Please provide a number greater than 1 :)");
+        System.out.print(">");
+        int input = scanner.nextInt();
+        int total = 0;
+        for (int i = 1; i <= input; i++) {
+            total += i;
+        }
+        System.out.println("The total from 1 to " + input + " is: " + total);
+
+    }
+}
